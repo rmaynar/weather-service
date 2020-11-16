d10fa65f274815323fa988b2e207702a6a733e1e
rmaynar <rmaynar@gmail.com>
2020-11-16 11:43:05 +0100

    added ResponseEntity instead of DTO

--- src/main/java/com/globant/weatherservice/controller/WeatherController.java
+++ src/main/java/com/globant/weatherservice/controller/WeatherController.java
@@ -6,6 +6,7 @@ import com.globant.weatherservice.utils.DateUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,45 +23,42 @@ public class WeatherController {
     private WeatherDataService weatherDataService;
 
     @PostMapping("/weather")
-    public WeatherData saveWeatherData(@RequestBody WeatherData weatherData, HttpServletResponse response){
+    public ResponseEntity<WeatherData> saveWeatherData(@RequestBody WeatherData weatherData){
 
         //Check if previously exists
         log.info("Checking for previous data");
         if(weatherData.getId() != 0 && weatherDataService.findById(weatherData.getId()).isPresent()){
             log.error("Weather data already exists!");
-            response.setStatus(HttpStatus.BAD_REQUEST.value());
-            return null;
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
 
-        response.setStatus(HttpStatus.CREATED.value());
-        return weatherDataService.save(weatherData);
+        WeatherData weathersaved = weatherDataService.save(weatherData);
+        return new ResponseEntity<>(weathersaved, HttpStatus.CREATED);
     }
 
     @GetMapping("/weather")
-    public List<WeatherData> getAllWeatherData(@RequestParam(required = false) String date
-            , HttpServletResponse response) throws ParseException {
+    public ResponseEntity<List> getAllWeatherData(@RequestParam(required = false) String date) throws ParseException {
         if(date != null){
             log.info("Returning all weather data filtered by date: " + date);
             Date inputDate = DateUtils.convertStrToDate(date, DateUtils.DATE_PATTERN_YYYY_MM_DD);
             log.info("Java formatted date: " + inputDate);
             List<WeatherData> filteredResult = weatherDataService.findAllByDate(inputDate);
             if(CollectionUtils.isEmpty(filteredResult)){
-                response.setStatus(HttpStatus.NOT_FOUND.value());
+                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
             }
-            return filteredResult;
+            return new ResponseEntity<>(filteredResult, HttpStatus.OK);
         }
         log.info("Returning all weather data");
-        return weatherDataService.findAll();
+        return new ResponseEntity<>(weatherDataService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/weather/temp")
-    public Object getMaxAndMinTemperaturePerLocation(@RequestParam String startDate, @RequestParam String endDate
+    public ResponseEntity<List> getMaxAndMinTemperaturePerLocation(@RequestParam String startDate, @RequestParam String endDate
             , HttpServletResponse response) throws ParseException {
         Date start = null;
         Date end = null;
         if(startDate == null && endDate == null){
-            response.setStatus(HttpStatus.BAD_REQUEST.value());
-            return null;
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
         if(startDate != null){
             start = DateUtils.convertStrToDate(startDate, DateUtils.DATE_PATTERN_YYYY_MM_DD);
@@ -68,12 +66,13 @@ public class WeatherController {
         if(endDate != null){
             end = DateUtils.convertStrToDate(endDate, DateUtils.DATE_PATTERN_YYYY_MM_DD);
         }
-        return weatherDataService.findAllByDateBetween(start, end);
+        return new ResponseEntity<>(weatherDataService.findAllByDateBetween(start, end), HttpStatus.OK);
     }
 
     @DeleteMapping("/eliminar")
-    public void deleteAllRecords(){
+    public ResponseEntity deleteAllRecords(){
         log.info("Deleting all the records");
         weatherDataService.deleteAll();
+        return new ResponseEntity(null, HttpStatus.OK);
     }
 }
